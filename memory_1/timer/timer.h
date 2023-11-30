157938567c6b798cb81624dd8c2976e8f407a8ec
Bro8u <betttttt@yandex.ru>
2023-11-30 15:58:58 +0300

    rubbish

--- memory_1/timer/timer.h
+++ memory_1/timer/timer.h
@@ -1,7 +1,41 @@
+#include <iostream>
+#include <chrono>
+#include <string>
+
 class Timer {
 public:
-    bool Expired() const;
+    Timer(std::chrono::milliseconds t) : t_(t) {
+        start_ = std::chrono::steady_clock::now();
+    }
+    bool Expired() const {
+        if (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start_) <= t_) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+private:
+    std::chrono::steady_clock::time_point start_;
+    std::chrono::milliseconds t_;
 };
 
 class TimeMeasurer {
+public:
+    TimeMeasurer(std::stringstream& stream) : ostream_(stream) {
+        start_ = std::chrono::steady_clock::now();
+    }
+    ~TimeMeasurer() {
+        
+        //std::chrono::duration<double, std::milli> em = std::chrono::steady_clock::now() - start_;
+        // std::chrono::milliseconds tim = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start_);
+        int ans = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start_).count();
+        std::stringstream a;
+        a << std::to_string(ans);
+        // std::string ansStr = a.str();
+        a;
+    }
+private:
+    std::chrono::steady_clock::time_point start_;
+    std::stringstream& ostream_;    
 };
